e814fc62b008a242f2855797d2adb3e29fd8ac40
Bruce Taylor <bruce.taylor@avios.com>
2020-04-24 18:32:16 +0100

    Updated the contact form.

---
+++ src/_data/contactform.js
@@ -0,0 +1,10 @@
+module.exports = {
+  botMessage: "Don’t fill this out if you're human.",
+  labelName: "Your name",
+  labelEmail: "Your email",
+  message: {
+    label: "Message",
+    placeholder: "Please write your message here..."
+  },
+  labelButton: "Send message"
+}
