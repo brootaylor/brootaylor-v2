9371cd9deacd609346c2b6eeed26054ab2dcd446
Bruce Taylor <bruce.taylor@avios.com>
2020-04-28 17:00:19 +0100

    Added service worker preamble

--- src/_data/site.js
+++ src/_data/site.js
@@ -11,7 +11,7 @@ module.exports = {
   display: 'standalone',
   rootUrl: process.env.URL || '',
   environment: process.env.ELEVENTY_ENV,
-  logo: '/assets/images/common/avios-logo.png',
+  logo: '/assets/images/common/logo.png',
   theme_color_android: '#212227',
   theme_color_ios: '#212227',
   background_color_android: '#ffffff',
