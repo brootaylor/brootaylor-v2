2ef2ce3664f8120e98d8c735b38ac34f57598331
Bruce Taylor <brootaylor@gmail.com>
2022-04-07 14:08:57 +0100

    Article and message content update

--- src/_data/messages.js
+++ src/_data/messages.js
@@ -2,11 +2,11 @@ module.exports = {
   info: {
     modifier: 'message--info',
     message: "I'm redesigning & redeveloping my website in the open.",
-    ctaUrl: '/writing/2020-02-22/redesigning-in-the-open',
-    ctaLabel: 'More about this.'
+    ctaUrl: '/writing/2022-02-22/redesigning-in-the-open',
+    ctaLabel: 'More about this.',
   },
   alert: {
     modifier: 'message--alert',
-    message: "Oops! We've got a problem here."
+    message: "Oops! We've got a problem here.",
   }
 }
