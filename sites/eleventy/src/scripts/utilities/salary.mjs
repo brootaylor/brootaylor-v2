ae633176ffbcf46cd1cbf44d410732a256b8f115
Bruce Taylor <brootaylor@gmail.com>
2023-03-22 23:13:48 +0000

    JS console labelling change

--- sites/eleventy/src/scripts/utilities/salary.mjs
+++ sites/eleventy/src/scripts/utilities/salary.mjs
@@ -5,13 +5,13 @@
 // Works out my minimum annual salary
 const annualSalary = () => {
   const salary = new Intl.NumberFormat('en-uk', { style: 'currency', currency: 'GBP' }).format((Math.PI * `1${'0'.repeat(5)}` / 3.69595 | 0))
-  console.info('Annual salary (min): ', salary)
+  console.info('Annual salary (min):', salary)
 }
 
 // Works out my minimum day rate (outside IR35)
 const dayRateOutside = () => {
   const rateOut = new Intl.NumberFormat('en-uk', { style: 'currency', currency: 'GBP' }).format((Math.PI * `2${'0'.repeat(2)}` / 1.795 | 0))
-  console.info('Day rate - outside IR35 - (min): ', rateOut)
+  console.info('Day rate - outside IR35 - (min):', rateOut)
 }
 
 // Works out my minimum day rate (inside IR35)
