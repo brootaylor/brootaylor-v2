e878316701b8e130331f1d1221086b63fb54736f
Bruce Taylor <brootaylor@gmail.com>
2023-05-22 22:14:45 +0100

    Updated console minimum annual salary

--- sites/eleventy/src/scripts/utilities/salary.mjs
+++ sites/eleventy/src/scripts/utilities/salary.mjs
@@ -4,19 +4,19 @@
 
 // Works out my minimum annual salary
 const annualSalary = () => {
-  const salary = new Intl.NumberFormat('en-uk', { style: 'currency', currency: 'GBP' }).format((Math.PI * `1${'0'.repeat(5)}` / 3.69595 | 0))
+  const salary = new Intl.NumberFormat('en-uk', { style: 'currency', currency: 'GBP' }).format((Math.PI * `1${'0'.repeat(5)}` / 3.92695 | 0)) // This equals £80,000
   console.info('Annual salary (min):', salary)
 }
 
 // Works out my minimum day rate (outside IR35)
 const dayRateOutside = () => {
-  const rateOut = new Intl.NumberFormat('en-uk', { style: 'currency', currency: 'GBP' }).format((Math.PI * `2${'0'.repeat(2)}` / 1.795 | 0))
+  const rateOut = new Intl.NumberFormat('en-uk', { style: 'currency', currency: 'GBP' }).format((Math.PI * `2${'0'.repeat(2)}` / 1.795 | 0)) // This equals £350
   console.info('Day rate - outside IR35 - (min):', rateOut)
 }
 
 // Works out my minimum day rate (inside IR35)
 const dayRateInside = () => {
-  const rateIn = new Intl.NumberFormat('en-uk', { style: 'currency', currency: 'GBP' }).format((Math.PI * `2${'0'.repeat(2)}` / 1.395 | 0))
+  const rateIn = new Intl.NumberFormat('en-uk', { style: 'currency', currency: 'GBP' }).format((Math.PI * `2${'0'.repeat(2)}` / 1.395 | 0)) // This equals £450
   console.info('Day rate - inside IR35 - (min):', rateIn)
 }
 
