0cd2f1afc35199424b772c2c1d57deb4c2203d66
Bruce Taylor <brootaylor@gmail.com>
2023-09-05 15:38:44 +0100

    Changed the global messages logic to be more dynamic at build time.

--- sites/eleventy/src/_data/messages.js
+++ sites/eleventy/src/_data/messages.js
@@ -1,12 +1,12 @@
 module.exports = {
   info: {
     modifier: 'message--info',
-    message: 'I posted something on my blog recently',
+    // message: 'I\'m making some changes to my website at the moment. Sorry if it looks a little weird!',
     // ctaUrl: '/writing/2023-05-31/the-climate-crisis-what-to-make-of-it',
     // ctaLabel: 'The "Climate Crisis": What to Make of It?',
   },
   alert: {
     modifier: 'message--alert',
-    message: "Oops! We've got a problem here.",
+    // message: "Oops! We've got a problem here.",
   }
 }
